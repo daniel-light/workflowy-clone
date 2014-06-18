7da963f2340911abe82ab8004c0379ab16d080d4
Daniel Light <dsl7v3@mail.missouri.edu>
2014-06-18 18:37:28 -0400

    Create User model

---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ActiveRecord::Base
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  before_validation do
+    self.session_token ||= SecureRandom.urlsafe_base64(32)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = find_by(email: email)
+
+    user if user.try(:is_password?, password)
+  end
+
+  attr_reader :password
+
+  def password=(secret)
+    self.password_digest = BCrypt::Password.create(secret)
+    @password = secret
+  end
+
+  def is_password?(secret)
+    BCrypt::Password.new(password_digest).is_password?(secret)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(32)
+    save!
+    session_token
+  end
+end
