6f17317a240f5cde9dc7554508426302c9aff8fe
Daniel Light <dsl7v3@mail.missouri.edu>
2014-06-20 21:24:17 -0400

    Add basic sessions logic
    Create SessionsController, #new, #create, #destroy, add to
    application.html

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.find_by_credentials(user_params[:email], user_params[:password])
+
+    if user
+      sign_in!(user)
+      render json: user
+    else
+      @user = User.new(user_params)
+      flash.now.alert = 'Invalid username or password'
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out!
+    redirect_to new_session_url
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
+
+  resource :session, only: [:new, :create, :destroy]
 end
