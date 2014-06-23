3022f8425303a1f6e87fc52d8b4b051d162e6c99
Daniel Light <dsl7v3@mail.missouri.edu>
2014-06-22 21:53:54 -0400

    Remove edit_item_url from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'root#index'
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  resources :items, except: [:new] do
+  resources :items, except: [:new, :edit] do
     member do
       post action: :create
     end
