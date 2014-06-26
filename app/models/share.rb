96dbe32b7a8df58e383b7b1cb2fc04b3fb99c8e1
Daniel Light <dsl7v3@mail.missouri.edu>
2014-06-26 17:05:15 -0400

    Don't allow self sharing

--- app/models/share.rb
+++ app/models/share.rb
@@ -2,6 +2,7 @@ class Share < ActiveRecord::Base
   validates :item_id, presence: true
   validates :user_id, uniqueness: {scope: :item_id}
   validates :can_edit, inclusion: {in: [false, true]}
+  validate :no_self_sharing
 
   before_validation do
     self.can_edit = false if can_edit.nil?
@@ -11,4 +12,12 @@ class Share < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :item
+
+  private
+
+  def no_self_sharing
+    if !item || user_id == item.user_id
+      errors.add(:user_id, 'can\'t share with yourself')
+    end
+  end
 end
--- spec/models/share_spec.rb
+++ spec/models/share_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Share, :type => :model do
 
   context 'validations' do
     it 'should be valid with a user_id and an item_id' do
-      expect(create(:item).shares.new(user_id: 1)).to be_valid
+      expect(create(:item).shares.new(user_id: 3)).to be_valid
     end
 
     it { should validate_presence_of(:item_id) }
@@ -19,14 +19,18 @@ RSpec.describe Share, :type => :model do
     end
 
     it 'should be able to have multiple users per item' do
-      create(:share, user_id: 1, item_id: 1)
-      expect(build(:share, user_id: 2, item_id: 1)).to be_valid
+      create(:share, user_id: 2, item_id: 1)
+      expect(build(:share, user_id: 3, item_id: 1)).to be_valid
     end
 
     it 'should automatically set a nil can_edit' do
       share = build(:share, can_edit: nil)
       share.valid?
       expect(share.can_edit).to be false
+    end
+
+    it 'should not let you share with yourself' do
+      expect(build(:share, user_id: 1, item_id: 1)).not_to be_valid
     end
   end
 
