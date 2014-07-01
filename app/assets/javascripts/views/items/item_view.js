38d9bc095adde8049d538c306dfa089782caa6d3
Daniel Light <dsl7v3@mail.missouri.edu>
2014-07-01 16:30:07 -0400

    Move item view events to separate file

--- app/assets/javascripts/views/items/item_view.js
+++ app/assets/javascripts/views/items/item_view.js
@@ -1,7 +1,9 @@
 ;(function(Workflowy) {
   "use strict";
 
-  Workflowy.Views.Item = Backbone.View.extend({
+  Workflowy.Views.Item = Workflowy.Views.Item || Backbone.View.extend();
+  _.extend(Workflowy.Views.Item.prototype, {
+
     tagName: 'li',
     template: JST['items/item'],
 
@@ -17,16 +19,6 @@
 
       this.bindShortcuts();
       this.listenTo(this.model, 'change', this.render);
-    },
-
-    events: {
-      'click .collapser': 'toggleCollapsed', // select this more precisely?
-      'input .title': 'changeTitle',
-      'focus .notes': 'expandNotes',
-      'blur .notes': 'collapseNotes',
-      'input .notes': 'changeNotes',
-      'focus p': 'activateShortcuts',
-      'blur p': 'disableShortcuts'
     },
 
     render: function() {
@@ -109,92 +101,5 @@
       window.getSelection().removeAllRanges();
       window.getSelection().addRange(range);
     },
-
-    toggleCollapsed: function() {
-      event.stopPropagation();
-      this.model.toggleCollapsed();
-    },
-
-    changeTitle: function(event) {
-      event.stopPropagation();
-      this.model.title($(event.currentTarget).text());
-    },
-
-    changeNotes: function(event) {
-      event.stopPropagation();
-      this.model.notes(event.currentTarget.innerText); //TODO firefox support
-    },
-
-    expandNotes: function(event) {
-      event.stopPropagation();
-      event.currentTarget.innerHTML = this.model.escape('notes');
-    },
-
-    collapseNotes: function(event) {
-      event.stopPropagation();
-      event.currentTarget.innerHTML = this.model.shortenedNotes();
-    },
-
-    bindShortcuts: function() {
-      var key = function(keys, f) {
-        window.key(keys, this.model.cid, f);
-      }.bind(this);
-
-      key('return', this.shortcutNewItem.bind(this));
-      key('shift + ctrl + right, tab', this.shortcutIndent.bind(this));
-      key('shift + ctrl + left, shift + tab', this.shortcutOutdent.bind(this));
-      key('shift + return', this.shortcutSwapField.bind(this));
-    },
-
-    activateShortcuts: function(event) {
-      event.stopPropagation();
-      key.setScope(this.model.cid);
-    },
-
-    disableShortcuts: function(event) {
-      event.stopPropagation();
-      key.setScope('all');
-    },
-
-    shortcutNewItem: function(event) {
-      if (!this.isBeingEdited('title')) return;
-      event.preventDefault();
-
-      var newItem = new Workflowy.Models.Item({
-        parent_id: this.model && this.model.parent_id,
-        uuid: Workflowy.generateUUID()
-      });
-
-      this.model.collection.insertAt(newItem, this.model.index() + 1);
-
-      if (newItem.view) {
-        newItem.view.focus();
-      }
-      else {
-        this.listenToOnce(newItem, 'viewCreated', function(){
-          newItem.view.focus();
-        });
-      }
-    },
-
-    shortcutSwapField: function(event) {
-      event.preventDefault();
-
-      this.isBeingEdited('title') ? this.focus('notes') : this.focus('title');
-    },
-
-    shortcutIndent: function(event) {
-      event.preventDefault();
-      if (!this.isBeingEdited('title')) return;
-
-      this.model.indent();
-    },
-
-    shortcutOutdent: function(event) {
-      event.preventDefault();
-      if (!this.isBeingEdited('title')) return;
-
-      this.model.outdent();
-    }
   });
 })(Workflowy);
---
+++ app/assets/javascripts/views/items/item_view_events.js
@@ -0,0 +1,104 @@
+;(function(Workflowy) {
+  "use strict";
+
+  Workflowy.Views.Item = Workflowy.Views.Item || Backbone.View.extend();
+  _.extend(Workflowy.Views.Item.prototype, {
+
+    events: {
+      'click .collapser': 'toggleCollapsed', // select this more precisely?
+      'input .title': 'changeTitle',
+      'focus .notes': 'expandNotes',
+      'blur .notes': 'collapseNotes',
+      'input .notes': 'changeNotes',
+      'focus p': 'activateShortcuts',
+      'blur p': 'disableShortcuts'
+    },
+
+    toggleCollapsed: function() {
+      event.stopPropagation();
+      this.model.toggleCollapsed();
+    },
+
+    changeTitle: function(event) {
+      event.stopPropagation();
+      this.model.title($(event.currentTarget).text());
+    },
+
+    changeNotes: function(event) {
+      event.stopPropagation();
+      this.model.notes(event.currentTarget.innerText); //TODO firefox support
+    },
+
+    expandNotes: function(event) {
+      event.stopPropagation();
+      event.currentTarget.innerHTML = this.model.escape('notes');
+    },
+
+    collapseNotes: function(event) {
+      event.stopPropagation();
+      event.currentTarget.innerHTML = this.model.shortenedNotes();
+    },
+
+    activateShortcuts: function(event) {
+      event.stopPropagation();
+      key.setScope(this.model.cid);
+    },
+
+    disableShortcuts: function(event) {
+      event.stopPropagation();
+      key.setScope('all');
+    },
+
+    bindShortcuts: function() {
+      var key = function(keys, f) {
+        window.key(keys, this.model.cid, f);
+      }.bind(this);
+
+      key('return', this.shortcutReturn.bind(this));
+      key('shift + ctrl + right, tab', this.shortcutIndent.bind(this));
+      key('shift + ctrl + left, shift + tab', this.shortcutOutdent.bind(this));
+      key('shift + return', this.shortcutSwapField.bind(this));
+    },
+
+    shortcutReturn: function(event) {
+      if (!this.isBeingEdited('title')) return;
+      event.preventDefault();
+
+      var newItem = new Workflowy.Models.Item({
+        parent_id: this.model && this.model.parent_id,
+        uuid: Workflowy.generateUUID()
+      });
+
+      this.model.collection.insertAt(newItem, this.model.index() + 1);
+
+      if (newItem.view) {
+        newItem.view.focus();
+      }
+      else {
+        this.listenToOnce(newItem, 'viewCreated', function(){
+          newItem.view.focus();
+        });
+      }
+    },
+
+    shortcutSwapField: function(event) {
+      event.preventDefault();
+
+      this.isBeingEdited('title') ? this.focus('notes') : this.focus('title');
+    },
+
+    shortcutIndent: function(event) {
+      event.preventDefault();
+      if (!this.isBeingEdited('title')) return;
+
+      this.model.indent();
+    },
+
+    shortcutOutdent: function(event) {
+      event.preventDefault();
+      if (!this.isBeingEdited('title')) return;
+
+      this.model.outdent();
+    }
+  });
+})(Workflowy);
