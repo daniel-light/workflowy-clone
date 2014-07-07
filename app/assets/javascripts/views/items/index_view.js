fcf561e3cc47676cafd7dab0e1430c315eec477a
Daniel Light <dsl7v3@mail.missouri.edu>
2014-07-07 16:39:22 -0400

    Add plus for creating a new item if the index is empty

--- app/assets/javascripts/views/items/index_view.js
+++ app/assets/javascripts/views/items/index_view.js
@@ -6,21 +6,42 @@
     template: JST['items/index'],
 
     initialize: function() {
-      this.$el.addClass('page')
+      this.$el.addClass('page');
 
       this.sublist = new Workflowy.Views.List({collection: this.collection});
       this.listenTo(this.collection, 'add remove sort', this.render);
     },
 
+    events: {
+      'click .new': 'addInitialItem'
+    },
+
     render: function() {
-      this.$el.html(this.template({items: this.collection}))
-      this.$el.children('.padded').append(this.sublist.render().$el)
+      this.$el.html(this.template({items: this.collection}));
+      this.$el.children('.padded').append(this.sublist.render().$el);
+
+      if (this.collection.length === 0) {
+        this.$el.children('.padded').html('<a class="new">+</a>');
+      }
       return this;
     },
 
     remove: function() {
       this.sublist.remove();
       return Backbone.View.prototype.remove.apply(this, arguments);
+    },
+
+    addInitialItem: function() {
+      var newItem = this.collection.createAt(0);
+
+      if (newItem.view) {
+        newItem.view.focus();
+      }
+      else {
+        this.listenToOnce(newItem, 'viewCreated', function(){
+          newItem.view.focus();
+        });
+      }
     }
   });
 })(Workflowy);
